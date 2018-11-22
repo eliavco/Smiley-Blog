7cce6232689c0190377fba956fbbfec48471439f
Eliav <eliav.s.cohen@gmail.com>
2018-11-22 19:00:29 +0200

    Playing with GitHub

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+# trying to push
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR     = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
