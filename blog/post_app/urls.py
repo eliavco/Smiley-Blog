82e5516d8dc1fb45ed8cb864e470f1eb5e5b1cc5
Eliav <eliav.s.cohen@gmail.com>
2018-12-02 20:55:57 +0200

    urls.py Completed!

--- blog/post_app/urls.py
+++ blog/post_app/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path,re_path
 from post_app import views
 app_name = "post_app"
 
 urlpatterns = [
     path('',views.About.as_view(),name="about"),
+    path('list/',views.PostListView.as_view(),name="list"),
+    path('drafts/',views.DraftListView.as_view(),name="drafts"),
+    path('post/new/',views.CreatePostView.as_view(),name="new_post"),
+    re_path(r'^post/(?P<pk>\d+)/$',views.PostDetailView.as_view(),name="post_detail"),
+    re_path(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name="post_edit"),
+    re_path(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name="post_publish"),
+    re_path(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name="post_delete"),
+    re_path(r'^post/(?P<pk>\d+)/comment/$',views.add_comment_to_post,name="comment_new"),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve,name="comment_approve"),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name="comment_remove"),    
 ]
